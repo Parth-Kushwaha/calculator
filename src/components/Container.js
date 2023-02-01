f5c34f78053b23fb0e95ea3041b67bc0c35b32c0
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-01 10:53:59 +0530

    calculator

---
+++ src/components/Container.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div>
+      <h2>CALCULATOR</h2>
+      <div>
+        <button>CLEAR</button>
+        <button>C</button>
+        <button></button>
+      </div>
+    </div>
+  )
+}
+
+export default Container
