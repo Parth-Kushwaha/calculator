27cd723efbf743e0fca5477f511de70f5195166f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-07 18:21:54 +0530

    c

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Container.css'
 function Container() {
+    const [results,setResults]=useState("")
   return (
-    <div>
-      <h2>CALCULATOR</h2>
-      <div>
-        <button>CLEAR</button>
-        <button>C</button>
-        <button></button>
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className="previous-operand">123</div>
+        <div className="current-operand">1245</div>
       </div>
+        <button className='span-two'>AC</button>
+        <button>DEL</button>
+        <button>&divide;</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>&times;</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>&ndash;</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span-two'>=</button>
     </div>
   )
 }
