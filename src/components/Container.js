aef794fcde0eaf14f2a78d05b381cfb4210e0a1f
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-15 17:18:17 +0530

    c

--- src/components/Container.js
+++ src/components/Container.js
@@ -28,17 +28,17 @@ function Container() {
         <button name='2'  onClick={handleClick}>2</button>
         <button name='3'  onClick={handleClick}>3</button>
         <button name='*'  onClick={handleClick}>&times;</button>
-        <button  name='4' onClick={handleClick}>4</button>
+        <button name='4' onClick={handleClick}>4</button>
         <button name='5'  onClick={handleClick}>5</button>
         <button name='6'  onClick={handleClick}>6</button>
-        <button  name='-' onClick={handleClick}>&ndash;</button>
-        <button  name='7' onClick={handleClick}>7</button>
+        <button name='-' onClick={handleClick}>&ndash;</button>
+        <button name='7' onClick={handleClick}>7</button>
         <button name='8'  onClick={handleClick}>8</button>
         <button name='9'  onClick={handleClick}>9</button>
         <button name='+'  onClick={handleClick}>+</button>
         <button name='.'  onClick={handleClick}>.</button>
         <button name='0'  onClick={handleClick}>0</button>
-        <button  name='' onClick={evaluate} className='span-two'>=</button>
+        <button name='' onClick={evaluate} className='span-two'>=</button>
     </div>
   )
 }
