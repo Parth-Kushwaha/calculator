f8e7dcb0b5aef19aae3c4115cf4eb11e5d624ee4
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-15 17:20:13 +0530

    c

--- src/components/Container.js
+++ src/components/Container.js
@@ -23,11 +23,11 @@ function Container() {
       </div>
         <button id='clear' onClick={()=> setResult('')} className='span-two'>AC</button>
         <button id='backspace' onClick={()=> setResult(result.slice(0, -1))}>C</button>
-        <button name='/'  onClick={handleClick}>&divide;</button>
-        <button name='1'  onClick={handleClick}>1</button>
-        <button name='2'  onClick={handleClick}>2</button>
-        <button name='3'  onClick={handleClick}>3</button>
-        <button name='*'  onClick={handleClick}>&times;</button>
+        <button name='/' onClick={handleClick}>&divide;</button>
+        <button name='1' onClick={handleClick}>1</button>
+        <button name='2' onClick={handleClick}>2</button>
+        <button name='3' onClick={handleClick}>3</button>
+        <button name='*' onClick={handleClick}>&times;</button>
         <button name='4' onClick={handleClick}>4</button>
         <button name='5'  onClick={handleClick}>5</button>
         <button name='6'  onClick={handleClick}>6</button>
