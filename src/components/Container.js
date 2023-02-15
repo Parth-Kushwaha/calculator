d2d1aa1c20af4359123806c12cb7a8de6196a39e
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-02-15 16:44:16 +0530

    c

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,29 +1,36 @@
+import { useState } from 'react'
 import './Container.css'
 function Container() {
+
+  const [result, setResult]=useState('')
+
+  const handleClick=(e)=>{
+    setResult(result.concat(e.target.name))
+  }
   return (
     <div className='calculator-grid'>
       <div className='output'>
         <div className="previous-operand">123</div>
-        <div className="current-operand">1245</div>
+        <div className="current-operand">{result}</div>
       </div>
-        <button className='span-two'>AC</button>
-        <button>DEL</button>
-        <button>&divide;</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>&times;</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>&ndash;</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-        <button>.</button>
-        <button>0</button>
-        <button className='span-two'>=</button>
+        <button onClick={handleClick} className='span-two'>AC</button>
+        <button onClick={handleClick}>DEL</button>
+        <button onClick={handleClick}>&divide;</button>
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
+        <button onClick={handleClick}>3</button>
+        <button onClick={handleClick}>&times;</button>
+        <button onClick={handleClick}>4</button>
+        <button onClick={handleClick}>5</button>
+        <button onClick={handleClick}>6</button>
+        <button onClick={handleClick}>&ndash;</button>
+        <button onClick={handleClick}>7</button>
+        <button onClick={handleClick}>8</button>
+        <button onClick={handleClick}>9</button>
+        <button onClick={handleClick}>+</button>
+        <button onClick={handleClick}>.</button>
+        <button onClick={handleClick}>0</button>
+        <button onClick={handleClick} className='span-two'>=</button>
     </div>
   )
 }
